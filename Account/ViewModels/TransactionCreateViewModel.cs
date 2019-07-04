34e9f048c53ee878af2f752a98c99cf1fdd01abf
Kasper Juul Hermansen <hermansendev@gmail.com>
2019-07-04 19:01:19 +0000

    Feature/generify names

--- Account/ViewModels/TransactionCreateViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Account.ViewModels
-{
-    public class TransactionCreateViewModel
-    {
-        [Required] public decimal Amount { get; set; }
-    }
-}
--- ApplicationUnitTests/UnitTest1.cs
+++ ApplicationUnitTests/UnitTest1.cs
@@ -1,13 +1,12 @@
 using System;
 using Xunit;
 
-namespace AccountUnitTests
+namespace ApplicationUnitTests
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
-        {
-        }
+        { }
     }
 }
