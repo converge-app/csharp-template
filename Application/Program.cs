865440448cb28e6e7e9eb0a750f4837914c0d7d7
kjuulh <hermansendev@gmail.com>
2019-09-07 12:32:55 +0200

    Update URLS

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,25 +3,30 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Application {
-    public class Program {
-        public static void Main (string[] args) {
-            CreateWebHostBuilder (args).Build ().Run ();
+namespace Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder (string[] args) {
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
 
-            var assemblyName = typeof (Startup).GetTypeInfo ().Assembly.FullName;
+            var assemblyName = typeof(Startup).GetTypeInfo().Assembly.FullName;
 
-            return WebHost.CreateDefaultBuilder (args)
-                .UseUrls ("http://0.0.0.0:5000")
-                .ConfigureLogging ((hostingContext, logging) => {
-                    logging.AddConfiguration (hostingContext.Configuration.GetSection ("Logging"));
-                    logging.AddConsole ();
-                    logging.AddDebug ();
-                    logging.AddEventSourceLogger ();
+            return WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:80")
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
                 })
-                .UseStartup (assemblyName);
+                .UseStartup(assemblyName);
         }
     }
 }
