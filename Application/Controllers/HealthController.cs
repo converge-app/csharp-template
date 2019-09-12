e2ced8e3ba2627d1a03843a47b3684726b08ee20
hermansen <hermansendev@gmail.com>
2019-09-12 17:12:15 +0200

    Add validation

--- Application/Controllers/HealthController.cs
+++ Application/Controllers/HealthController.cs
@@ -1,16 +1,37 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     public class HealthController : Controller
     {
+        private readonly IHttpClientFactory clientFactory;
+
+        public HealthController(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
+
         // GET api/health/ping
         [Produces("application/json")]
         [HttpGet("ping")]
-        public ActionResult Ping()
+        public async Task<ActionResult> PingAsync()
         {
-            return Ok(new { Message = "pong!" });
+//            var client = clientFactory.CreateClient ();
+//
+//            var request = new HttpRequestMessage (HttpMethod.Get, "http://localhost:5000/api/health/ping");
+//
+//            var response = await client.SendAsync (request);
+//
+//            if (response.IsSuccessStatusCode) {
+            return Ok(new {Message = "pong!"});
+//            } else {
+//                return BadRequest ("Cannot connect to localhost:5000");
+//            }
         }
     }
 }
